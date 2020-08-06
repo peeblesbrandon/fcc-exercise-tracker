a71504b533dbfd8d563db3d429e6a2077acbe6d3
Glitch (exercise-tracker-bsp) <none>
2020-08-06 22:03:55 +0000

    Completed exercise tracker on Glitch

---
+++ models/Exercise.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const exerciseSchema = new mongoose.Schema({
+  userId: {type: String, required: true},
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: {type: Date, required: false, default: Date.now}
+});
+
+module.exports = mongoose.model('Exercise', exerciseSchema);
